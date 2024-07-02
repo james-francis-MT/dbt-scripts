d2718576bb5b5e5c65cd781a953ea53538369170
James Francis <james.francis@madetech.com>
2024-07-02 11:49:14 +0100

    add script to push cf params

---
+++ parameter_store/put_parameters_gov.py
@@ -0,0 +1,54 @@
+import argparse
+import boto3
+from botocore import exceptions
+
+parser = argparse.ArgumentParser()
+parser.add_argument('application', type=str, help='The name of the application')
+parser.add_argument('env', type=str, help='The name of the environment')
+parser.add_argument('service', type=str, help='The name of the service')
+args = parser.parse_args()
+
+APPLICATION = args.application
+ENVIRONMENT = args.env
+SERVICE = args.service
+FILENAME = "ssm_parameters.txt"
+
+ssm_path_root = f"/copilot/{APPLICATION}/{ENVIRONMENT}/"
+
+ssm = boto3.client('ssm')
+
+with open(FILENAME, 'r') as f:
+    ssm_data = f.read()
+
+class Param:
+    def __init__(self, name, value) -> None:
+        self.name = name
+        self.value = value
+
+    def name_as_key(self, app, env, service):
+        return f"/copilot/{app}/{env}/{service}/{self.name}"
+        
+
+params: list[Param] = []
+for line in ssm_data.splitlines():
+    [name, *value] = line.split(": ", 1)
+    value = value[0] if value else None
+    params.append(Param(name, value))
+
+for param in params:
+     print(f"Setting SSM parameter with name: {param.name_as_key(APPLICATION, ENVIRONMENT, SERVICE)}")
+     try:
+         ssm.put_parameter(Name=param.name_as_key(APPLICATION, ENVIRONMENT, SERVICE),
+                       Value=param.value,
+                       Tags=[
+                           {"Key": "copilot-application", "Value": f"{APPLICATION}"},
+                           {"Key": "copilot-environment", "Value": f"{ENVIRONMENT}"}
+                       ],
+                       Type="String",
+                       Overwrite=False)
+     except exceptions.ClientError as e:
+         print(f"{param.name} parameter already exists, will not Overwrite")
+         pass
+ 
+     print(f"Set SSM parameter with name: {param.name}")
+
